c1890db7426a822e6506ffd585099f89fbb89caa
santomy1999 <santomy2110@gmail.com>
2023-09-07 23:27:54 +0530

    Client side:seperated the recieve_and_display_coordinates function to smaller functions. Added recieve data code that needs to be tested.\n ServerSide : Tried adding a recieve data code. Didn't work.

--- client.py
+++ client.py
@@ -4,6 +4,8 @@ import websockets
 import threading
 import pyautogui  # Used to get screen dimensions
 
+#server ip
+SERVER_URI = "ws://192.168.1.11:8765"
 # Constants for the screen size
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
@@ -27,39 +29,61 @@ def update_coordinates(x_coordinate, y_coordinate, prev_x, prev_y):
     place_object(x_coordinate, y_coordinate)
     trace_coordinates(x_coordinate, y_coordinate, prev_x, prev_y)
 
-async def receive_and_display_coordinates():
-    uri = "ws://192.168.1.5:8765"  # Replace SERVER_IP with the server's IP address or domain name
-    async with websockets.connect(uri) as websocket:
-        prev_x, prev_y = None, None  # Initialize previous coordinates
+async def send_data(data):
+    async with websockets.connect(SERVER_URI) as websocket:
+        await websocket.send(data)
+
+
+def send_mouse_coordinates():
+    while True:
+        x, y = pyautogui.position()
+        coordinates = f"x={x},y={y}"
+        send_data(coordinates)
+
+async def receive_data():
+    async with websockets.connect(SERVER_URI) as websocket:
         while True:
             coordinates = await websocket.recv()
-            parts = coordinates.split(',')
+            yield coordinates
 
-            # Initialize variables
-            x_coordinate = None
-            y_coordinate = None
+def extract_coordinates(data):
+    parts = data.split(',')
 
-            # Iterate through the parts to extract the coordinates
-            for part in parts:
-                key, value = part.split('=')
-                if key == 'x':
-                    x_coordinate = int(value)
-                elif key == 'y':
-                    y_coordinate = int(value)
-            
-            if x_coordinate is not None and y_coordinate is not None:
-                # Scale the coordinates to match the screen size
-                x_coordinate = int(x_coordinate / X_SCALE_FACTOR)
-                y_coordinate = int(y_coordinate / Y_SCALE_FACTOR)
-                if prev_x is not None and prev_y is not None:
-                    update_coordinates(x_coordinate, y_coordinate, prev_x, prev_y)
-                    
-                prev_x, prev_y = x_coordinate, y_coordinate
+    # Initialize variables
+    x_coordinate = None
+    y_coordinate = None
+
+    # Iterate through the parts to extract the coordinates
+    for part in parts:
+        key, value = part.split('=')
+        if key == 'x':
+            x_coordinate = int(value)
+        elif key == 'y':
+            y_coordinate = int(value)
+
+    return x_coordinate, y_coordinate
+
+async def display_object():
+    
+    prev_x, prev_y = None, None  # Initialize previous coordinates
+
+    async for data in receive_data():
+        x_coordinate, y_coordinate = extract_coordinates(data)
+        
+        if x_coordinate is not None and y_coordinate is not None:
+            # Scale the coordinates to match the screen size
+            x_coordinate = int(x_coordinate / X_SCALE_FACTOR)
+            y_coordinate = int(y_coordinate / Y_SCALE_FACTOR)
+            if prev_x is not None and prev_y is not None:
+                update_coordinates(x_coordinate, y_coordinate, prev_x, prev_y)
+
+            prev_x, prev_y = x_coordinate, y_coordinate
+
 
 def start_websocket_thread():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(receive_and_display_coordinates())
+    loop.run_until_complete(display_object())
 
 # Create the Tkinter window with the specified dimensions
 window = tk.Tk()
@@ -81,6 +105,13 @@ place_object(x_coordinate, y_coordinate)
 websocket_thread = threading.Thread(target=start_websocket_thread)
 websocket_thread.daemon = True
 websocket_thread.start()
+
+# Create a thread for sending mouse coordinates
+mouse_coordinates_thread = threading.Thread(target=send_mouse_coordinates)
+mouse_coordinates_thread.daemon = True  # Daemonize the thread (so it doesn't prevent program exit)
+
+# Start the thread
+mouse_coordinates_thread.start()
 
 # Start the Tkinter main loop
 window.mainloop()
